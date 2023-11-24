6ff7c5e4e846a1473eb4d34aa4b356306807db21
Julian Schoen <j.schoen@mail.com>
2023-11-24 18:39:59 +0100

    Update application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,9 @@ export default function page() {
 
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <Image
-            className="relative "
+            className=""
             src="/font2.png"
-            alt="Next.js Logo"
+            alt="Main Logo"
             width={650}
             height={100}
             priority
