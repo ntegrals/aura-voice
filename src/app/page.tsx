4552fe22a4d1cca889ee9795f54c5b874ee1a377
TotalLag <phanx068@gmail.com>
2024-11-14 01:54:39 -0600

    feat(ui): implement responsive layout and enhance animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import AssistantButton from "@/components/AssistantButton/AssistantButton";
 import Image from "next/image";
 
-export default function page() {
+export default function Page() {
   return (
-    <div>
+    <>
+      {/* Desktop Layout */}
       <div className="hidden md:block">
         <main className="flex min-h-screen flex-col justify-center items-center p-24">
           <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
             <Image
-              className=""
               src="/font2.png"
               alt="Main Logo"
               width={650}
               height={100}
+              priority
             />
           </div>
         </main>
@@ -21,14 +22,25 @@ export default function page() {
           <AssistantButton />
         </div>
       </div>
-      <div className="md:hidden">
-        <main className="flex min-h-screen flex-col justify-center items-center p-24">
-          <div className=" font-semibold text-xl relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            Please use a desktop browser for the best experience. (I'm working
-            on a mobile version)
+
+      {/* Mobile Layout */}
+      <div className="md:hidden flex min-h-screen flex-col items-center">
+        <div className="flex-1 flex flex-col items-center justify-center w-full px-4">
+          <div className="relative flex place-items-center w-[80vw] mb-16 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            <Image
+              src="/font2.png"
+              alt="Main Logo"
+              width={300}
+              height={50}
+              priority
+              className="w-full h-auto"
+            />
           </div>
-        </main>
+          <div className="w-full flex justify-center">
+            <AssistantButton />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
